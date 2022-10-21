a5f660dff6d296846d28944613654f8da616445b
Elaine <elainedoud@gmail.com>
2022-10-21 15:12:46 -0400

    commit tree f3b31df1bb49845a6ac1c0d8a9c96270b7f9b3b5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+
+    function submitData(name,email) {
+        const newObject = {
+            name: name,
+            email: email,
+        }  
+        return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(newObject)
+     })
+        .then(response => response.json())
+        .then(data => {
+            document.body.textContent = data.id
+        }
+    )}
+
+    
